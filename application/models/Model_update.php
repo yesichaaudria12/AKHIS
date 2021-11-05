9a5c28caa9bdbcda4ab94459d689b9c3c8016272
sahrul <sahrulprograming@gmail.com>
2021-11-05 14:28:24 +0700

    fix

--- application/models/Model_update.php
+++ application/models/Model_update.php
@@ -1,17 +1,19 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
+ini_set('date.timezone', 'Asia/Jakarta');
 
 class Model_update extends CI_Model
 {
-    public function obat($id_obat = null){
-        if ($_FILES['gambar']['name']){
+    public function obat($id_obat = null)
+    {
+        if ($_FILES['gambar']['name']) {
             $_POST += [
                 'gambar' => upload_foto('gambar', './assets/img/obat'),
             ];
         }
-        $_POST['harga'] = str_replace('.','',$this->input->post('harga'));
+        $_POST['harga'] = str_replace('.', '', $this->input->post('harga'));
         $this->db->where('id_obat', $id_obat);
-        $this->db->update('obat', array_slice($_POST,1));
+        $this->db->update('obat', array_slice($_POST, 1));
         $result = $this->db->affected_rows();
         if ($result > 0) {
             $this->session->set_flashdata('message', '<div class="alert alert-success alert-dismissible fade show text-center" role="alert">
@@ -27,11 +29,13 @@ class Model_update extends CI_Model
             redirect(current_url());
         }
     }
-    public function baca_pesan($id = null){
+    public function baca_pesan($id = null)
+    {
         $id_user = $this->session->userdata('id');
         $this->db->query("UPDATE chat SET status = 'dibaca' WHERE dari = '$id' AND kepada = '$id_user'");
     }
-    public function resep_obat($id_pasien = null, $id = null){
+    public function resep_obat($id_pasien = null, $id = null)
+    {
         $id_resep = $this->input->post('id_resep');
         $data = [
             'id_obat' => $this->input->post('id_obat'),
@@ -56,9 +60,10 @@ class Model_update extends CI_Model
             redirect("dokter/ubah/detail_resep/$id_pasien/$id_resep");
         }
     }
-    public function upload_bukti(){
-        if ($_FILES['bukti_bayar']['name']){
-            if ($this->input->post('id_dp')){
+    public function upload_bukti()
+    {
+        if ($_FILES['bukti_bayar']['name']) {
+            if ($this->input->post('id_dp')) {
                 $_POST += [
                     'bukti_bayar' => upload_foto('bukti_bayar', './assets/img/bukti_pembayaran'),
                 ];
@@ -71,27 +76,27 @@ class Model_update extends CI_Model
                 ];
                 $this->db->update('pembayaran', $data);
                 $result = $this->db->affected_rows();
-                if ($result > 0){
+                if ($result > 0) {
                     $this->session->set_flashdata('message', '<div class="alert alert-success alert-dismissible fade show text-center" role="alert">
                 <strong>Berhasil Upload Bukti pembayaran, tunggu konfirmasi admin yah!!</strong>
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                 </div>');
-                redirect(current_url());
+                    redirect(current_url());
                 } else {
                     $this->session->set_flashdata('message', '<div class="alert alert-danger alert-dismissible fade show text-center" role="alert">
                     <strong>gagal Upload Bukti pembayaran!</strong>
                     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                     </div>');
                     redirect(current_url());
-                    }
-            }else{
+                }
+            } else {
                 $this->session->set_flashdata('message', '<div class="alert alert-danger alert-dismissible fade show text-center" role="alert">
                 <strong>anda belum memilih Metode Pembayaran!</strong>
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                 </div>');
                 redirect(current_url());
             }
-        }else{
+        } else {
             $this->session->set_flashdata('message', '<div class="alert alert-danger alert-dismissible fade show text-center" role="alert">
             <strong>anda belum upload bukti!</strong>
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
@@ -99,16 +104,17 @@ class Model_update extends CI_Model
             redirect(current_url());
         }
     }
-    public function password($role, $id){
-        $cek = $this->db->get_where($role, ['id_'.$role => $id])->row_array();
-        if (password_verify($this->input->post('password_lama'), $cek['password'])){
-            if(!password_verify($this->input->post('password_baru'), $cek['password'])){
-                $password = password_hash($this->input->post('password_baru'),PASSWORD_DEFAULT);
+    public function password($role, $id)
+    {
+        $cek = $this->db->get_where($role, ['id_' . $role => $id])->row_array();
+        if (password_verify($this->input->post('password_lama'), $cek['password'])) {
+            if (!password_verify($this->input->post('password_baru'), $cek['password'])) {
+                $password = password_hash($this->input->post('password_baru'), PASSWORD_DEFAULT);
                 $this->db->set('password', $password);
-                $this->db->where('id_'.$role, $id);
+                $this->db->where('id_' . $role, $id);
                 $this->db->update($role);
                 $result = $this->db->affected_rows();
-                if ($result > 0){
+                if ($result > 0) {
                     $this->session->set_flashdata('message', '<div class="alert alert-success alert-dismissible fade show text-center" role="alert">
                     <strong>Password berhasil dirubah!</strong>
                     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
@@ -121,12 +127,13 @@ class Model_update extends CI_Model
                     </div>');
                     redirect(current_url());
                 }
-            } $this->session->set_flashdata('message', '<div class="alert alert-danger alert-dismissible fade show text-center" role="alert">
+            }
+            $this->session->set_flashdata('message', '<div class="alert alert-danger alert-dismissible fade show text-center" role="alert">
             <strong>Password Baru sama dengan password lama!</strong>
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
             </div>');
             redirect(current_url());
-        }else{
+        } else {
             $this->session->set_flashdata('message', '<div class="alert alert-danger alert-dismissible fade show text-center" role="alert">
             <strong>Password lama salah!</strong>
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
@@ -134,11 +141,12 @@ class Model_update extends CI_Model
             redirect(current_url());
         }
     }
-    public function profile($role, $id){
+    public function profile($role, $id)
+    {
         $this->db->where("id_$role", $id);
         $this->db->update($role, $_POST);
         $result = $this->db->affected_rows();
-        if ($result > 0){
+        if ($result > 0) {
             $this->session->set_flashdata('message', '<div class="alert alert-success alert-dismissible fade show text-center" role="alert">
             <strong>Profile berhasil dirubah!</strong>
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
@@ -152,26 +160,27 @@ class Model_update extends CI_Model
             redirect(current_url());
         }
     }
-    public function rubah_pesanan($id_resep, $status){
-        if ($status == 'dikirim'){
+    public function rubah_pesanan($id_resep, $status)
+    {
+        if ($status == 'dikirim') {
             $this->db->set('no_resi', $this->input->post('no_resi'));
         }
         $this->db->set('status', $status);
         $this->db->where('id_resep', $id_resep);
         $this->db->update('pesanan');
         $result = $this->db->affected_rows();
-            if ($result > 0){
-                $this->session->set_flashdata('message', '<div class="alert alert-success alert-dismissible fade show text-center" role="alert">
+        if ($result > 0) {
+            $this->session->set_flashdata('message', '<div class="alert alert-success alert-dismissible fade show text-center" role="alert">
                 <strong>Status pesanan berhasil dirubah!</strong>
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                 </div>');
-                redirect('admin/lihat/pesanan/'.$status);
-            } else {
-                $this->session->set_flashdata('message', '<div class="alert alert-danger alert-dismissible fade show text-center" role="alert">
+            redirect('admin/lihat/pesanan/' . $status);
+        } else {
+            $this->session->set_flashdata('message', '<div class="alert alert-danger alert-dismissible fade show text-center" role="alert">
                 <strong>Status pesanan masih sama!</strong>
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                 </div>');
-                redirect(current_url());
-            }
+            redirect(current_url());
+        }
     }
 }
--- application/views/admin/v_tambah.php
+++ application/views/admin/v_tambah.php
@@ -4,7 +4,6 @@
 			padding-inline: 4rem;
 		}
 	}
-
 </style>
 <div class="page-wrapper">
 	<div class="page-content">
@@ -21,50 +20,45 @@
 					<div class="basic-form">
 						<?= form_open_multipart(current_url()); ?>
 						<?php $i = 0;
-                            foreach ($columns as $c) : ?>
-						<div class="form-group row my-2">
-							<label
-								class="col-lg-3 col-form-label text-dark"><?= ucwords(str_replace("_", " ", $c->name)); ?></label>
-							<div class="col-lg-9">
-								<?php if (isset($input) && $input[$i] == 'options') : ?>
-								<select class="form-control input-rounded" id="sel1" name="<?= $c->name; ?>">
-									<option value="<?= set_value($c->name); ?>">
-										<?= set_value($c->name) ? set_value($c->name) :"Silahkan Pilih ". ucwords(str_replace('_',' ',$c->name)); ?>
-									</option>
-									<?php foreach($options as $o): ?>
-									<option value="<?= $o[$value]; ?>"><?= $o[$list]; ?></option>
-									<?php endforeach; ?>
-								</select>
-								<?= form_error($c->name, '<small class="pl-3 text-danger">', '</small>'); ?>
-								<?php elseif(isset($input) && $input[$i] == 'textarea') :?>
-								<div class="form-floating">
-									<textarea class="form-control" name="<?= $c->name; ?>" id="<?= $c->name; ?>"
-										style="height: 100px"
-										placeholder="Masukan <?= str_replace("_", " ", $c->name); ?>"
-										<?= isset($add_atribut) ? $add_atribut[$i] : ""; ?>
-										value="<?= set_value($c->name); ?>"><?= set_value($c->name); ?></textarea>
-									<label for="<?= $c->name; ?>"><?= str_replace("_", " ", $c->name); ?></label>
-									<?= form_error($c->name, '<small class="pl-3 text-danger">', '</small>'); ?>
-								</div>
-								<?php else : ?>
-								<div class="input-group">
-									<?php if($c->name == 'harga'): ?>
-									<div class="input-group-prepend"><span class="input-group-text"
-											id="basic-addon1">Rp.</span></div>
+						foreach ($columns as $c) : ?>
+							<div class="form-group row my-2">
+								<label class="col-lg-3 col-form-label text-dark"><?= ucwords(str_replace("_", " ", $c->name)); ?></label>
+								<div class="col-lg-9">
+									<?php if (isset($input) && $input[$i] == 'options') : ?>
+										<select class="form-control input-rounded" id="sel1" name="<?= $c->name; ?>">
+											<option value="<?= set_value($c->name); ?>">
+												<?= set_value($c->name) ? set_value($c->name) : "Silahkan Pilih " . ucwords(str_replace('_', ' ', $c->name)); ?>
+											</option>
+											<?php foreach ($options as $o) : ?>
+												<option value="<?= $o[$value]; ?>"><?= $o[$list]; ?></option>
+											<?php endforeach; ?>
+										</select>
+										<?= form_error($c->name, '<small class="pl-3 text-danger">', '</small>'); ?>
+									<?php elseif (isset($input) && $input[$i] == 'textarea') : ?>
+										<div class="form-floating">
+											<textarea class="form-control" name="<?= $c->name; ?>" id="<?= $c->name; ?>" style="height: 100px" placeholder="Masukan <?= str_replace("_", " ", $c->name); ?>" <?= isset($add_atribut) ? $add_atribut[$i] : ""; ?> value="<?= set_value($c->name); ?>"><?= set_value($c->name); ?></textarea>
+											<label for="<?= $c->name; ?>"><?= str_replace("_", " ", $c->name); ?></label>
+											<?= form_error($c->name, '<small class="pl-3 text-danger">', '</small>'); ?>
+										</div>
+									<?php else : ?>
+										<div class="input-group">
+											<?php if ($c->name == 'harga') : ?>
+												<div class="input-group-prepend"><span class="input-group-text" id="basic-addon1">Rp.</span></div>
+											<?php endif; ?>
+											<input type="<?= isset($type) ? $type[$i] : "text"; ?>" class="form-control input-rounded" name="<?= $c->name; ?>" id="<?= $c->name; ?>" placeholder="Masukan <?= str_replace("_", " ", $c->name); ?>" <?= isset($add_atribut) ? $add_atribut[$i] : ""; ?> value="<?= set_value($c->name); ?>">
+											<script>
+												$('#harga').mask('#.##0', {
+													reverse: true
+												});
+											</script>
+										</div>
+										<?= form_error($c->name, '<small class="pl-3 text-danger">', '</small>'); ?>
 									<?php endif; ?>
-									<input type="<?= isset($type) ? $type[$i] : "text"; ?>"
-										class="form-control input-rounded" name="<?= $c->name; ?>" id="<?= $c->name; ?>"
-										placeholder="Masukan <?= str_replace("_", " ", $c->name); ?>"
-										<?= isset($add_atribut) ? $add_atribut[$i] : ""; ?>
-										value="<?= set_value($c->name); ?>">
-								</div>
-								<?= form_error($c->name, '<small class="pl-3 text-danger">', '</small>'); ?>
-								<?php endif; ?>
 
+								</div>
 							</div>
-						</div>
 						<?php $i++;
-                            endforeach; ?>
+						endforeach; ?>
 						<div class="row text-left justify-content-end">
 							<div class="col-lg-9 my-2 pr-4 text-right">
 								<button type="submit" class="btn btn-rounded btn-success px-5">Tambah</button>
